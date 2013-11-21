aec1fa181e01690fe1db0b11a51a87831149a01f
Carlos Lopez <carloshlopez@gmail.com>
2013-11-20 21:22:10 -0500

    routes fix ooops

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Bap::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'games#theroy'
+  root 'games#theory'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
